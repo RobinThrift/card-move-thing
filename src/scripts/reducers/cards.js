9c22a676b0658c931e473e665e571faae923173e
leMaik <an@maik.tk>
2015-11-15 00:16:02 +0100

    🐛 fix card updating

--- src/scripts/reducers/cards.js
+++ src/scripts/reducers/cards.js
@@ -6,16 +6,20 @@ function sortCards(c1, c2) {
     return c2.sort - c1.sort;
 }
 
-export function cardReducer(cards = new List(), action) {
+export function cardReducer(cards = new Map(), action) {
     switch (action.type) {
         case ADD_CARD:
-            return cards.push(action.card).sort(sortCards);
+            return cards.set(action.card.id, action.card).value().sort(sortCards);
         case UPDATE_CARD:
-            return cards.set(action.id, action.card).sort(sortCards);
+            return cards.map((card) => {
+                if (card.id === action.id) {
+                    return action.card;
+                }
+                return card;
+            });
         case REMOVE_CARD:
             return cards.delete(action.id, action.card).sort(sortCards);
         default:
             return cards;
     }
 }
-

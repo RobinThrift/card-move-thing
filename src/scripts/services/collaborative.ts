881880b0d85ba1e4a54ca1fdef1bbcac435c97bc
leMaik <an@maik.tk>
2015-11-15 23:27:25 +0100

    ➕ added sharejs client

---
+++ src/scripts/services/collaborative.ts
@@ -0,0 +1,44 @@
+import sharejs from 'share';
+import {BCSocket} from './node_modules/browserchannel/dist/bcsocket';
+import json from 'ot-json0';
+import jsondiff from 'jsondiff-share-ops';
+
+declare class Document<T> {
+    publish: (state: T) => any;
+}
+
+let createDocument = async function<T>(name: string, initialState: T, onChange: (x: T) => any) {
+    let socket = new BCSocket(null, {reconnect: true});
+    let sjs = new sharejs.Connection(socket);
+    let doc = sjs.get('card-move-thing', name);
+    let ctx = null;
+
+    return new Promise<Document>((resolve, reject) => {
+        doc.subscribe(() => onChange(ctx.getSnapshot()));
+
+        doc.whenReady(() => {
+            if (!doc.type) {
+                doc.create(json.type.name, initialState);
+            }
+
+            ctx = doc.createContext();
+            ctx.addListener({}, '', () => {}); //workaround for json0 api bug
+            doc.on('after op', () => onChange(ctx.getSnapshot()));
+        });
+
+        return {
+            publish: (state: T) => {
+                if (!ctx) {
+                    throw new Error('ShareJS context not ready yet');
+                }
+
+                let diff = jsondiff.diff(ctx.getSnapshot(), state);
+                if (diff.length > 0) {
+                    ctx.submitOp(diff);
+                }
+            }
+        };
+    });
+};
+
+export createDocument;

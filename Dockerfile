9eb213e17c88702bf9ac5f7fb91e089fa50776af
leMaik <an@maik.tk>
2015-11-25 13:22:50 +0100

    💻 added docker-compose for redis and node

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
 FROM node:4.2.1-slim
 
 ADD package.json /src/package.json
-RUN cd /src; npm install
-
 ADD server/package.json /src/server/package.json
-RUN cd /src/server; npm install
+RUN cd /src; npm install
 
 COPY . /src
 RUN cd /src; node ./node_modules/gulp/bin/gulp.js dist
--- server/redisBackend.js
+++ server/redisBackend.js
@@ -3,10 +3,10 @@ var redis = require('redis');
 
 module.exports = function() {
     var memorydb = livedb.memory();
-    var redisClient1 = redis.createClient(6379, 'localhost');
-    var redisClient2 = redis.createClient(6379, 'localhost');
+    var redisClient1 = redis.createClient(process.env.REDIS_PORT || 6379, process.env.REDIS_ADDR || 'localhost');
+    var redisClient2 = redis.createClient(process.env.REDIS_PORT || 6379, process.env.REDIS_ADDR || 'localhost');
     var driver = livedb.redisDriver(memorydb, redisClient1, redisClient2);
-    
+
     var backend = livedb.client({db: memorydb, driver: driver});
     backend.redis = redisClient1;
     backend.db = memorydb;
